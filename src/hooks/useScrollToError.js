18558d6391331d567815520faf8297beec3eb9b2
Pedro Zunino <peter@rootstrap.com>
2020-03-22 12:11:16 -0300

    Fix scroll hook

--- src/hooks/useScrollToError.js
+++ src/hooks/useScrollToError.js
@@ -2,9 +2,11 @@ import { useEffect } from 'react'
 
 export default (ref, ...errors) =>
   useEffect(() => {
+    console.log('scrolling')
     const invalidInput = ref.current.querySelector("input[aria-invalid='true']")
     void invalidInput?.scrollIntoView({
       behavior: 'smooth',
       block: 'center',
     })
-  }, [ref, errors])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ref, ...errors])
