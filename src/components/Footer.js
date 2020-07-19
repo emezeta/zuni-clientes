d6fba1621c6c25376e3d3639792338da0cd8f11d
Pedro Zunino <peter@rootstrap.com>
2020-07-19 20:09:13 -0300

    Fix footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,23 +3,25 @@ import React, { memo } from 'react'
 import Button from '@material-ui/core/Button'
 
 const Footer = ({ newProduct, submit, disabled }) => (
-  <div className="mt-auto mb-4 d-flex align-items-center rounded">
-    <Button
-      disabled={disabled}
-      onClick={submit}
-      className="col mr-1 mr-md-3 py-md-4"
-      color="secondary"
-    >
-      REALIZAR LA COMPRA
-    </Button>
-    <Button
-      disabled={disabled}
-      onClick={newProduct}
-      className="col ml-1 ml-md-3 py-md-4"
-      color="primary"
-    >
-      AGREGAR PRODUCTO
-    </Button>
+  <div className="row">
+    <div className="col col-md-6 offset-md-3 mb-4">
+      <Button
+        disabled={disabled}
+        onClick={submit}
+        className="col-6 py-3"
+        color="secondary"
+      >
+        REALIZAR LA COMPRA
+      </Button>
+      <Button
+        disabled={disabled}
+        onClick={newProduct}
+        className="col-6 py-3"
+        color="primary"
+      >
+        AGREGAR PRODUCTO
+      </Button>
+    </div>
   </div>
 )
 
