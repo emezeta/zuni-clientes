46d03d8baa3180d2b0297b4d3a150acdc28fba8c
Pedro Zunino <peter@rootstrap.com>
2020-07-19 10:53:51 -0300

    Styling fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,21 +6,21 @@ const Footer = ({ newProduct, submit, disabled }) => (
   <div className="mt-auto mb-4 d-flex align-items-center rounded">
     <Button
       disabled={disabled}
-      onClick={newProduct}
-      className="col mr-1 mr-md-3 py-md-4"
-      variant="contained"
-      color="primary"
-    >
-      AGREGAR PRODUCTO
-    </Button>
-    <Button
-      disabled={disabled}
       onClick={submit}
-      className="col ml-1 ml-md-3 py-md-4"
+      className="col mr-1 mr-md-3 py-md-4"
       variant="contained"
       color="secondary"
     >
       REALIZAR LA COMPRA
+    </Button>
+    <Button
+      disabled={disabled}
+      onClick={newProduct}
+      className="col ml-1 ml-md-3 py-md-4"
+      variant="contained"
+      color="primary"
+    >
+      AGREGAR PRODUCTO
     </Button>
   </div>
 )
